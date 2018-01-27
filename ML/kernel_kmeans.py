efb52c812cfbc3d1391322248d0d3f0d64e5685b
anthonychen288 <32977192+anthonychen288@users.noreply.github.com>
2018-01-27 16:52:28 +0800

    revision of kernel kmeans
    Totally revise the procedure of kernel k-means

--- ML/kernel_kmeans.py
+++ ML/kernel_kmeans.py
@@ -1,8 +1,9 @@
 import sys
 import numpy as np
+import matplotlib.pyplot as plt
+
 from math import exp
-from bokeh.plotting import figure, show, output_file
-from bokeh.layouts import gridplot
+from sklearn.metrics import confusion_matrix
 
 def show_usage():
 	print("[Usage]\t%s [data_path] [dataset] [clusters]" %(sys.argv[0]))
@@ -24,129 +25,120 @@ def read_data(path):
 	return np.array(data), np.array(target)
 
 def IsConverged(new, old):
-	if abs(new - old) < 1e-3:
+	if abs(new - old) < 1e-7:
 		return True
 	else:
 		return False
 
-class KernelKMeans:
+class KernelKMeans():
 	"""docstring for KernelKMeans"""
-	def __init__(self, n_clusters, n_feats):
-		self.kernel_type = "poly"
-		self.n_clusters = n_clusters
-		self.n_feats = n_feats
+	def __init__(self, n_cluster, kernel="poly", c=1.0, d=1.0, sigma=1.0):
+		self.kernel_type = kernel
+		self.n_clusters = n_cluster
 		self.tsse_ = 0.0
-		self.plots = []
-		self.colors = np.array([c for c in ('#00f', '#0f0', '#f00', '#0ff', '#f0f', '#ff0')])
+		self.c = c
+		self.d = d
+		self.sigma = 1.0
+		self.labels_ = None
+		self.tsse_ = 0.0
 
 	@property
 	def plot_colors(self):
 		return self.colors
 
-	def kernel(self, x, c=0.0, d=1.0, sigma=1.0):
+	def kernel(self, x=np.array([])):
 		'''
 		c & d are for polynomial kernel, 
 		and when c=0.0, d=1.0, it becomes a linear kernel
 		sigma is for rbf kernel
 		'''
-		n_data = len(x)
-		kernel_x = [[0.0]*n_data for i in range(n_data)]
+		n_data = x.shape[0]
+		n_feats = x.shape[1]
+		kernel_x = np.zeros((n_data, n_data))
 		# polynomial kernel
 		if self.kernel_type == 'poly':
 			for i in range(n_data):
 				for j in range(n_data):
-					kernel_x[j][i] = kernel_x[i][j] = (np.array(x[i]).dot(np.array(x[j])) + c)**d
+					kernel_x[j][i] = kernel_x[i][j] = (np.array(x[i]).dot(np.array(x[j])) + self.c)**self.d
 		# RBF kernel
 		elif self.kernel_type == 'rbf':
 			for i in range(n_data):
 				for j in range(n_data):
 					sqr_err = 0.0
-					for d in range(self.n_feats):
+					for d in range(n_feats):
 						sqr_err += (x[i][d] - x[j][d])**2
-					kernel_x[i][j] = kernel_x[j][i] = exp(-(sqr_err / (2.0 * (sigma**2) )))
+					kernel_x[i][j] = kernel_x[j][i] = exp(-(sqr_err / (2.0 * (self.sigma**2) )))
 		else:
 			raise ValueError("Unknown kernel: %s" %(self.kernel_type))
 		
 		return kernel_x
 
-	def fit(self, x, kernel="poly", c=1.0, sigma=1.0):
-		n_data = len(x)
-		self.kernel_type = kernel
-		kernel_x = self.kernel(x, c=c, sigma=sigma)
+	def fit(self, x=np.array([]), max_iter=50):
+		n_data = x.shape[0]
+		kernel_x = self.kernel(x)
 		# random initial
-		init = np.random.random_integers(0, n_data-1, self.n_clusters)
-		self.centroids = [kernel_x[i] for i in init]
-
-		results = [0] * n_data
+		labels = np.random.randint(self.n_clusters, size=n_data)
+		
 		iterates = 0
+		tsse = np.inf
 		# max iter
-		while iterates < 50:
-			new_tsse_ = 0.0
-			# update membership
-			for i in range(n_data):
-				dist = [0.0] * self.n_clusters
-				sq_err = float("inf")
-				for k in range(self.n_clusters):
-					for d in range(n_data):
-						dist[k] += (kernel_x[i][d] - self.centroids[k][d])**2
-					if dist[k] < sq_err:
-						results[i] = k
-						sq_err = dist[k]
-				new_tsse_ += sq_err
-			# save clustering results
-			p = figure(title="iter %d" %(iterates), plot_width=400, plot_height=400)
-			p.scatter(x[:, 0], x[:, 1], color=self.plot_colors[results].tolist())
-			# p.diamond(np.array(self.centroids)[:, 0], np.array(self.centroids)[:, 1], size=20)
-			self.plots.append(p)
-			# update centroids
-			new_centroids = [[0.0]*n_data for i in range(self.n_clusters)]
-			cluster_size = [0.0] * self.n_clusters
-			for i in range(n_data):
-				cluster_size[results[i]] += 1
-				for d in range(n_data):
-					new_centroids[results[i]][d] += kernel_x[i][d]
-
+		while iterates < max_iter:
+			membership = np.zeros((n_data, self.n_clusters))
+			# compute distance
+			old_tsse = tsse
+			cluster_size = [0.0 for i in range(self.n_clusters)]
 			for k in range(self.n_clusters):
-				for d in range(n_data):
-					new_centroids[k][d] /= cluster_size[k]
+				idx, = np.where(labels == k)
+				size = len(idx)
+				gram_matrix = kernel_x[idx, idx]
+				membership[:, k] += -2 * np.sum(kernel_x[:, idx], axis=1) / float(size)
+				membership[:, k] += np.sum(gram_matrix / float(size * size))
+
+			tsse = np.sum(membership)
+			old_labels = labels
+			# update assignment
+			labels = membership.argmin(axis=1)
+			# assignment convergence
+			"""
+			change = len(np.where(old_labels == labels))
+			if not change:
+				break
+			"""
+			# TSSE convergence
 			
-			iterates += 1
-			# determine convergence using tsse_
-			if IsConverged(new_tsse_, self.tsse_):
+			if IsConverged(tsse, old_tsse):
 				break
 
-			self.tsse_ = new_tsse_
-			self.centroids = new_centroids
-			print("Iterations\t%d" %iterates)
-			print("TSSE\t%f" %(self.tsse_))
-			print("===============================")
+			iterates += 1
 			
-
-		p = figure(title="iter %d" %(iterates), plot_width=400, plot_height=400)
-		p.scatter(x[:, 0], x[:, 1], color=self.plot_colors[results].tolist())
-		# p.diamond(np.array(self.centroids)[:, 0], np.array(self.centroids)[:, 1], size=20)
-		self.plots.append(p)
+		self.labels_ = labels
+		self.tsse_ = tsse
+		print("Iterations\t%d" %(iterates+1))
+		print("TSSE\t%f" %(self.tsse_))
+		print("===============================")
 		
-		return np.array(results)
+		return self
 
-	def plot_cluster(self, data_name, x_data, y_data, n_cols=4, width=400, height=400):
-		p = figure(title="True cluster", plot_width=width, plot_height=height)
-		p.scatter(x_data[:, 0], x_data[:, 1], color=self.plot_colors[y_data].tolist())
-		self.plots.append(p)
-		grid = gridplot(self.plots, ncols=n_cols, plot_width=width, plot_height=height)
-		output_file("kernel_kmeans_%d_clusters_%s.html"%(self.n_clusters, data_name), \
-			title="kernelkmeans clustering iterations")
-		show(grid)
+	@property
+	def labels_(self):
+		return self.labels_
+	
+	@property
+	def tsse_(self):
+		return self.tsse_
 
 if len(sys.argv) != 4:
 	show_usage()
 	sys.exit()
+
 k = int(sys.argv[3])
 x_data, y_data = read_data("%s\\%s" %(sys.argv[1], sys.argv[2]))
-n_feats = x_data.shape[1]
 
-kkmeans = KernelKMeans(n_clusters=k, n_feats=n_feats)
-print("Kernel KMeans clustering: %d clusters rbf kernel" %(k))
-results = kkmeans.fit(x_data, kernel="rbf", sigma=3.0)
-kkmeans.plot_cluster(data_name=sys.argv[2], x_data=x_data, y_data=y_data)
-
+for s in range(20):
+	kkm = KernelKMeans(n_cluster=k, kernel="rbf", sigma=float(s+1 / 2.0))
+	print("Kernel KMeans clustering: %d cluster rbf kernel with sigma=%.2f" %(k, (s+1)/2.0))
+	kkm.fit(x_data, max_iter=100)
+	print(confusion_matrix(y_data, kkm.labels_))
+	plt.scatter(x_data[:, 0], x_data[:, 1], c=kkm.labels_)
+	plt.title("sigma %.2f" %((s+1) / 2.0))
+	plt.show()
